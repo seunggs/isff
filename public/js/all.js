6da91b654af4cf634aaf3437a95f5275b3dea0e7
Michael Chien <mike.chien@gmail.com>
2013-11-07 17:26:15 -0500

    Fixing up form - have ideal layout, needs more logic processing

--- public/js/all.js
+++ public/js/all.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 	
+$("[id$=email]").click(function(){
+	// this.parent is the containing div -> next is the names div
+	$(this).parent().next().show();
+})
+
 $("form").submit(function(){
 	var signup_form = $(this);
 	var email = $("input[name='email']",signup_form);
+	var fname = $("input[name='fname']",signup_form);
+	var lname = $("input[name='lname']",signup_form);
 	var alert_div = signup_form.next(".alert");
 
 	alert_div.hide();
@@ -12,6 +19,14 @@ $("form").submit(function(){
 	if (email.val() == "" || email.val() == "Your email address") {
 		alert_div.addClass("alert-error");
 		alert_div.html("That's not a valid email address!");
+		alert_div.show();
+	} else if (fname.val() == "" || fname.val() == "First name") {
+		alert_div.addClass("alert-error");
+		alert_div.html("Please enter your first name");
+		alert_div.show();		
+	} else if (lname.val() == "" || lname.val() == "Last name") {
+		alert_div.addClass("alert-error");
+		alert_div.html("Please enter your last name");
 		alert_div.show();
 	} else {
 		alert_div.addClass("alert-success");
