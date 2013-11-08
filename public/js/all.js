64f0d461e57bd6789e40a6454c00aecf15fe2957
Michael Chien <mike.chien@gmail.com>
2013-11-08 11:45:33 -0500

    Better messaging from back-end.

--- public/js/all.js
+++ public/js/all.js
@@ -5,41 +5,77 @@ $("[id$=email]").click(function(){
 	$(this).parent().next().show();
 })
 
-$("form").submit(function(){
+var feedback = function(div, msg, mode) {
+	div.html(msg);
+	div.removeClass("alert-info alert-success alert-error");
+	
+	if(mode == 0) // This is a success mode
+		div.addClass("alert-success");
+	if(mode == 1) // This is an error mode
+		div.addClass("alert-error");
+	if(mode == 2) // This is an info mode
+		div.addClass("alert-info");
+}
+
+$("[id$=signup]").submit(function(){
 	var signup_form = $(this);
 	var email = $("input[name='email']",signup_form);
 	var fname = $("input[name='fname']",signup_form);
 	var lname = $("input[name='lname']",signup_form);
-	var alert_div = signup_form.next(".alert");
+	var alert_div = signup_form.next("div.alert");
 
 	alert_div.hide();
 	alert_div.removeClass("alert-info alert-success alert-error");
-	alert_div.addClass("alert");
+
+
 
 	if (email.val() == "" || email.val() == "Your email address") {
-		alert_div.addClass("alert-error");
-		alert_div.html("That's not a valid email address!");
-		alert_div.show();
+		feedback(alert_div, "That's not a valid email address!", 1);
+		alert_div.slideDown("fast", function(){
+			// Option to do something to highlight more
+		});
 	} else if (fname.val() == "" || fname.val() == "First name") {
-		alert_div.addClass("alert-error");
-		alert_div.html("Please enter your first name");
-		alert_div.show();		
+		feedback(alert_div, "Please enter your first name.", 1);
+		alert_div.slideDown("fast", function(){
+			// Option to do something to highlight more
+		});
 	} else if (lname.val() == "" || lname.val() == "Last name") {
-		alert_div.addClass("alert-error");
-		alert_div.html("Please enter your last name");
-		alert_div.show();
+		feedback(alert_div, "Please enter your last name.", 1);
+		alert_div.slideDown("fast", function(){
+			// Option to do something to highlight more
+		});
 	} else {
 		alert_div.addClass("alert-success");
 		alert_div.html("Thanks for signing up!");
-		alert_div.show();
+		alert_div.slideDown("fast", function(){
+			// Option to do something to highlight more
+		});
 
 		var q = "/email?email=" + email.val();
+		q += "&fname=" + fname.val();
+		q += "&lname=" + lname.val();
 
 		$.get(q, function(data){
-			// add error handling
-			setTimeout(function(){
-				alert_div.hide();
-			}, 3000);
+			if(data){
+				if("e" in data){
+					switch (data.e){
+						case 0: // Successful signup of new email
+							break;
+
+						case 1: // Some error
+							feedback(alert_div, data.msg, data.e);
+							break;
+					}
+				} else {
+					// the email GET should always return something
+					// if nothing is returned, we couldn't hit the server
+					feedback(alert_div, "We're sorry, but we couldn't reach the server at this time. Please try again in a bit!", 2);
+				}
+			} else {
+				// the email GET should always return something
+				// if nothing is returned, we couldn't hit the server
+				feedback(alert_div, "We're sorry, but we couldn't reach the server at this time. Please try again in a bit!", 2);
+			}
 		})
 	}
 	
--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var https = require("https");
 var http = require("http");
 
 var app = express();
-app.use(express.logger());
+// app.use(express.logger());
 
 app.use(express.static(__dirname + '/public'));
 
@@ -29,43 +29,45 @@ app.get('/email', function(request, response) {
 	var query = url_parts.query;
 
 	if (validateEmail(query)){
-		var path = "/1.3/?method=listSubscribe";
-		// DEV key
-		path += "&apikey=f763c3e3b52322971d34e694d6b45f25-us6";
-		path += "&id=599e0cea16";
-		// PRODUCTION key
-		// path += "&apikey=419d7b769b3aba041aa80a0a7cd4edd1-us6";
-		// path += "&id=743f4bd26a";
-		path += "&email_address=" + query.email.toLowerCase();
-		path += "&double_optin=false";
-		path += "&update_existing=true";
+	// 	var path = "/1.3/?method=listSubscribe";
+	// 	// DEV key
+	// 	path += "&apikey=f763c3e3b52322971d34e694d6b45f25-us6";
+	// 	path += "&id=599e0cea16";
+	// 	// PRODUCTION key
+	// 	// path += "&apikey=419d7b769b3aba041aa80a0a7cd4edd1-us6";
+	// 	// path += "&id=743f4bd26a";
+	// 	path += "&email_address=" + query.email.toLowerCase();
+	// 	path += "&double_optin=false";
+	// 	path += "&update_existing=true";
 
-		var options = {
-			host: 'us6.api.mailchimp.com',
-			port: 443,
-			path: path,
-			method: 'POST',
-			headers: {
-				'Content-Length': 0
-			}
-		};
+	// 	var options = {
+	// 		host: 'us6.api.mailchimp.com',
+	// 		port: 443,
+	// 		path: path,
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Content-Length': 0
+	// 		}
+	// 	};
 
-		var req = https.request(options, function(res) {
-			console.log("statusCode: ", res.statusCode);
-			res.setEncoding('utf8');
-			res.on('data', function (chunk) {
-				if(chunk == "true")
-					response.send("Thank you for signing up!");
-				else{
-					response.send("Something went wrong.");
-				}
-			});
-		});
+	// 	var req = https.request(options, function(res) {
+	// 		console.log("statusCode: ", res.statusCode);
+	// 		res.setEncoding('utf8');
+	// 		res.on('data', function (chunk) {
+	// 			if(chunk == "true")
+	// 				response.send("Thank you for signing up!");
+	// 			else{
+	// 				response.send("Something went wrong.");
+	// 			}
+	// 		});
+	// });
 
 		req.on('error', function(e){
 			response.send("Something went wrong... " + e);
 		});
 		req.end();
+	} else {
+		response.json({e:1, msg:"This email address does not look valid."});
 	}
 
 });
